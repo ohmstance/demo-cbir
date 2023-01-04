e3cd65f439fa0335cd30db2e35ecbe19a67fa25c
Adam E. Aliasaa <adam.elyas99@gmail.com>
2023-01-04 23:11:38 +0800

    Add HTML GUI developed using Flask

---
+++ cbir.py
@@ -0,0 +1,252 @@
+import cv2
+import torch
+import os
+import numpy as np
+import pickle
+from torchvision import transforms
+from torchvision.transforms.functional import gaussian_blur
+from torchvision.models import mobilenet_v3_small
+from PIL import Image
+
+from tqdm.auto import tqdm
+from utils import image_show
+
+def update_features(db_path="features.pickle", img_dir="images"):
+    """Update features in database according to images in image directory
+    """
+    
+    # Get image filenames
+    filenames = [fn for fn in os.listdir("images") if os.path.isfile(f"images/{fn}")]
+
+    # Creates a list of dict in the format of ...
+    # [{'filename': <file name of image>, 'image': <numpy array of pixel values>}, ...]
+    images = []
+    for filename in filenames:
+        image_path = f"images/{filename}"
+        with Image.open(image_path) as img:
+            img = np.array(img, dtype=bool)
+        images.append({'filename': filename, 'image': img})
+        
+    for index, image in enumerate(images):
+        image = image['image']
+        image = image.astype(np.uint8) # Convert to grayscale
+
+        # Morphologically open image
+        kernel = np.ones((5, 5), np.uint8)
+        image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations=1)
+
+        image = image.astype(bool) # Convert to binary
+        image = Image.fromarray(image) # Convert to PIL object
+
+        # Resize to 224 x 224
+        image = image.resize((224, 224), resample=Image.Resampling.LANCZOS)
+
+        # Convert to RGB for input into CNN
+        image = image.convert('RGB')
+
+        # Add gaussian blur
+        image = gaussian_blur(image, (13, 13))
+
+        image = np.array(image) # Convert back to numpy array
+        images[index]['image'] = image
+
+    # Convert to tensor and normalize according to mean and standard deviation
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.8, 0.9, 0.9), (0.58, 0.5, 0.5))
+    ])
+
+    model = mobilenet_v3_small(weights="DEFAULT")
+    model.classifier = torch.nn.Identity() # model.classifier[:1] # torch.nn.Identity()
+    model.eval()
+    model = torch.jit.script(model) # JIT optimize model
+    model = torch.jit.optimize_for_inference(model) # Fuse batch norm + cnn layers
+        
+    features = {}
+    with torch.inference_mode():
+        for img in tqdm(images):
+            filename = img['filename']
+            image = img['image']
+
+            # Tensorize, normalize, and add batch dimension
+            image = transform(image)
+            image = image.unsqueeze(0)
+
+            # Extract features
+            feature = model(image)
+
+            # Remove batch dimension and convert features to numpy array
+            feature = feature.squeeze(0)
+            feature = feature.detach().cpu().numpy()
+
+            features.update({filename: feature})
+
+    with open(db_path, 'wb') as file:
+        pickle.dump(features, file)
+    
+def get_similar(image, db_path="features.pickle", img_dir="images"):
+    """Returns a list dict of of similar images with filenames
+    """
+    image = image.astype(np.uint8)
+    
+    # Morphologically open image
+    kernel = np.ones((5, 5), np.uint8)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations=1)
+
+    image = image.astype(bool) # Convert to binary
+    image = Image.fromarray(image) # Convert to PIL object
+
+    # Resize to 224 x 224
+    image = image.resize((224, 224), resample=Image.Resampling.LANCZOS)
+
+    # Convert to RGB for input into CNN
+    image = image.convert('RGB')
+
+    # Add gaussian blur
+    image = gaussian_blur(image, (13, 13))
+
+    image = np.array(image) # Convert back to numpy array
+
+    # Convert to tensor and normalize according to mean and standard deviation
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.8, 0.9, 0.9), (0.58, 0.5, 0.5))
+    ])
+    
+    model = mobilenet_v3_small(weights="DEFAULT")
+    model.classifier = torch.nn.Identity() # model.classifier[:1] # torch.nn.Identity()
+    model.eval()
+    model = torch.jit.script(model) # JIT optimize model
+    model = torch.jit.optimize_for_inference(model) # Fuse batch norm + cnn layers
+    
+    with torch.inference_mode():
+        # Tensorize, normalize and add batch dimension
+        image = transform(image)
+        image = image.unsqueeze(0)
+
+        # Extract features
+        feature = model(image)
+
+        # Remove batch dimension and convert features to numpy array
+        feature = feature.squeeze(0)
+        feature = feature.detach().cpu().numpy()
+
+    out_feature = feature
+    
+    # Load features of images in database
+    with open(db_path, 'rb') as file:
+        features = pickle.load(file)
+
+    # Calculate Euclidean difference between input feature and features in database
+    dist_index = []
+    for filename, feature in features.items():
+        dist = np.linalg.norm(out_feature-feature)
+        dist_index.append({'filename': filename, 'distance': dist})
+
+    # Sort by distance in ascending order
+    dist_index = sorted(dist_index, key = lambda x: x['distance'])
+    
+    filenames = [dist['filename'] for dist in dist_index]
+    distances = [dist['distance'] for dist in dist_index]
+    images = []
+
+    for dist in dist_index[0: 20]:
+        with Image.open(f"{img_dir}/{dist['filename']}") as img:
+            img = np.array(img, dtype=bool)
+            images.append(img)
+            
+    ret = []        
+    for filename, distance, image in zip(filenames, distances, images):
+        im = {
+            'filename': filename,
+            'image': image,
+            'distance': distance
+        }
+        ret.append(im)
+        
+    return ret
+
+def list_classes(db_path="features.pickle"):
+    with open(db_path, 'rb') as file:
+        features = pickle.load(file)
+        
+    classes = [filename.split('-')[0] for filename in features.keys()]
+    classes = sorted(list(set(classes)))
+    
+    return classes
+
+def evaluate_database(db_path="features.pickle"):
+    with open(db_path, 'rb') as file:
+        features = pickle.load(file)
+    
+    precisions = []
+    recalls = []
+    tps, fps, fns, tns = [], [], [], []
+
+    for filename, feature in features.items():
+        # Calculate Euclidean difference between input feature and features in database
+        dist_index = []
+        for filename_db, feature_db in features.items():
+            dist = np.linalg.norm(feature-feature_db)
+            dist_index.append({'filename': filename_db, 'distance': dist})
+
+        # Sort by distance in ascending order
+        dist_index = sorted(dist_index, key = lambda x: x['distance'])
+
+        # Obtain class string from input feature
+        img_class = filename.split('-')[0]
+
+        # Obtain class string from retrieved images' filename
+        ret_classes = [dist['filename'].split('-')[0] for dist in dist_index[:20]]
+
+        # Number of images that should be retrieved and was
+        tp = sum([img_class == ret_class for ret_class in ret_classes])
+
+        # Number of images that shouldn't be retrieved but was
+        fp = len(ret_classes) - tp
+
+        # Number of images that should be retrieved but wasn't
+        fn = sum([True for fn in features.keys() if img_class in fn]) - tp
+
+        # Number of images that shouldn't be retrieved and wasn't
+        tn = sum([True for fn in features.keys() if img_class not in fn]) - fp
+
+        tps.append(tp)
+        fps.append(fp)
+        fns.append(fn)
+        tns.append(tn)
+        # precisions.append(tp / (tp + fp))
+        # recalls.append(tp / (tp + fn))
+
+    tps, fps, fns, tns = sum(tps), sum(fps), sum(fns), sum(tns)
+    precision = tps / (tps + fps)
+    recall = tps / (tps + fns)
+    
+    return precision, recall
+
+def evaluate_search(in_class, retrieved, db_path="features.pickle"):
+    with open(db_path, 'rb') as file:
+        features = pickle.load(file)
+    
+    # Obtain class string from image path
+    img_class = in_class
+
+    # Obtain class string from retrieved images' filename
+    ret_classes = [r['filename'].split('-')[0] for r in retrieved[:20]]
+
+    # Number of images that should be retrieved and was
+    tp = sum([img_class == ret_class for ret_class in ret_classes])
+
+    # Number of images that shouldn't be retrieved but was
+    fp = len(ret_classes) - tp
+
+    # Number of images that should be retrieved but wasn't
+    fn = sum([True for fn in features.keys() if img_class in fn]) - tp
+
+    # Number of images that shouldn't be retrieved and wasn't
+    tn = sum([True for fn in features.keys() if img_class not in fn]) - fp
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    
+    return precision, recall
---
+++ gui_server.py
@@ -0,0 +1,96 @@
+import io
+import os
+import webbrowser
+import numpy as np
+from base64 import b64encode
+from PIL import Image
+from flask import Flask, request, render_template, redirect, url_for
+
+import cbir
+
+app = Flask(__name__, template_folder="templates")
+app.config.update(
+    TEMPLATES_AUTO_RELOAD=True
+)
+
+exiting = False
+
+@app.route("/", methods=['GET', 'POST'])
+def main_app():
+    # Home page
+    if request.method == 'GET':
+        return render_template("index.html")
+    
+    # POST request API for image search
+    if request.method == 'POST':
+        # Get image from form
+        file = request.files['search_img']
+        img = Image.open(file.stream)
+        
+        # Save image to memory and pass to template in base64 encoded PNG
+        mem_file = io.BytesIO()
+        img.save(mem_file, 'PNG')
+        search_img = "data:image/png;base64," + b64encode(mem_file.getvalue()).decode('ascii')
+        
+        # Finds similar images
+        img = np.array(img, dtype=bool)
+        sim = cbir.get_similar(img)
+        
+        # Organizes output into [[filename, image, distance], ...] for html template
+        retrieved = []
+        for r in sim:
+            filename = r['filename']
+            distance = f"{r['distance']:.3f}"
+            
+            img = Image.fromarray(r['image'])
+            mem_file = io.BytesIO()
+            img.save(mem_file, 'PNG')
+            img = "data:image/png;base64," + b64encode(mem_file.getvalue()).decode('ascii')
+            
+            retrieved.append((filename, img, distance))
+        
+        # Keyword args that will be passed to html template
+        html_vars = {
+            'classes': cbir.list_classes(),
+            'search_img': search_img,
+            'search_img_name': file.filename,
+            'retrieved': retrieved
+        }
+        
+        # If calculate precision recall is checked, do calculation according to class and add to dict
+        if request.form.get('calculate_pr', False):
+            in_class = request.form['select_class']
+            precision, recall = cbir.evaluate_search(in_class, sim)
+            html_vars.update({
+                'precision': precision,
+                'recall': recall
+            })
+        
+        # Renders html page
+        return render_template("index.html", **html_vars)
+
+# Updates features of images in directory into features.pickle
+@app.route("/update", methods=['GET'])
+def update_features():
+    cbir.update_features()
+    return redirect(url_for('main_app'))
+
+# Shuts down server
+@app.route("/shutdown", methods=['GET'])
+def shutdown():
+    global exiting
+    exiting = True
+    return "Done"
+
+# Teardown is after completing request -- this one shuts down server
+# There's no straight forward way to shut down a Flask server
+@app.teardown_request
+def teardown(exception):
+    if exiting:
+        os._exit(0)
+
+if __name__ == "__main__":
+    print("Open URL: http://localhost:8080/")
+    webbrowser.open('http://localhost:8080')
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
